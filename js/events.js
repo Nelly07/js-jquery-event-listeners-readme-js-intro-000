67a4415a4bc8b4f7430226119cf4bc6b4947b8f9
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-27 04:51:50 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!");
+  });
+}
+function frameIt(){
+  $('img').on('load',function(){
+    $("img").addClass('tasty');
+  });
+}
+function pressIt(){
+  $(document).on('keydown', function(key){
+    if (key.which==71){
+      alert("G  was pressed");
+    }
+  });
+}
+
+function submitIt(){
+  $('input').on('submit',funtion(){
+    alert("Your form is going to be submitted now.");
+  });
+}
+
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
