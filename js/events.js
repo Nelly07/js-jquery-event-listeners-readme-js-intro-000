ff0758e07f808250f266e125a8f0eb48f5ff733e
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-27 04:56:03 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,27 +1,27 @@
 //define functions here
-function getIt() {
-  $('p').on('click', function() {
-    alert('Hey!');
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!");
   });
 }
-
- function frameIt() {
-  $('img').on('load', function() {
+function frameIt(){
+  $('img').on('load',function(){
     $('img').addClass('tasty');
   });
 }
-
- function pressIt() {
-  $('input').on('keydown', function(key) {
-    if(key.which == 71){
-        alert('g was pressed');
+function pressIt(){
+  $(document).on('keydown', function(key){
+    if (key.which==71){
+      alert("G  was pressed");
     }
   });
 }
 
- function submitIt() {
-  $('form').on('submit', function() {
-    alert('Your form is going to be submitted now.');
+function submitIt(){
+  $("form").on("submit", function() {
+  if ($( "input:first" ).val() === "correct") {
+    alert('your form is going to be submitted now.');
+    return;
   });
 }
 
