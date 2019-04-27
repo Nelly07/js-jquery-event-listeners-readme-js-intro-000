4e73adedd0b7a9241d1f758cfd9fe74c379f907a
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-27 04:57:41 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -19,8 +19,7 @@ function pressIt(){
 
 function submitIt(){
   $("form").on("submit", function() {
-    alert('your form is going to be submitted now.');
-    return;
+    alert('Your form is going to be submitted now.');
   });
 }
 
